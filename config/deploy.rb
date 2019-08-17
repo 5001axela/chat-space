134c1f65b358c0d204cf3adfdae319ba745c9b0c
5001axela <axela0104@au.com>
2019-08-17 19:27:16 +0900

    update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,12 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.11.0"
+# config valid only for current version of Capistrano
+# capistranoのバージョンを記載。固定のバージョンを利用し続け、バージョン変更によるトラブルを防止する
+lock '~> 3.11.0'
 
-set :application, "chat-space"
-set :repo_url, "git@github.com:5001axela/chat-space.git"
+# Capistranoのログの表示に利用する
+set :application, 'chat-space'
+
+# どのリポジトリからアプリをpullするかを指定する
+set :repo_url,  'git@github.com:5001axela/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
@@ -12,7 +16,7 @@ set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/axela0104.pem']
+                  keys: ['~/.ssh/axela0104.pem']  ※例：~/.ssh/key_pem.pem
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
